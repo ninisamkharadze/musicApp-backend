3a51640766ee07f7ccd5a4a5fad385927ad4b305
ninisamkharadze <nini.samkharadze2012@gmail.com>
2025-07-11 11:08:38 -0800

    feat: connected to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'Nini19.06.2012.samkharadze',
+    database: 'musicapp',
+    autoLoadEntities: true,
+    synchronize: true
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
