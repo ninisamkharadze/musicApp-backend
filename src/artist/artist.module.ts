142b01bd6b6f57930d8669cbc5e73b42975bff96
ninisamkharadze <nini.samkharadze2012@gmail.com>
2025-07-24 04:33:54 -0800

    feat: add CRUD logic to repository structure

--- src/artist/artist.module.ts
+++ src/artist/artist.module.ts
@@ -3,10 +3,11 @@ import { ArtistService } from './artist.service';
 import { ArtistController } from './artist.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Artist } from './entities/artist.entity';
+import { ArtistRepository } from './repositories/artist.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Artist])],
   controllers: [ArtistController],
-  providers: [ArtistService],
+  providers: [ArtistService, ArtistRepository],
 })
 export class ArtistModule {}
--- src/artist/artist.service.ts
+++ src/artist/artist.service.ts
@@ -1,26 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { CreateArtistDto } from './dto/create-artist.dto';
 import { UpdateArtistDto } from './dto/update-artist.dto';
+import { ArtistRepository } from './repositories/artist.repository';
 
 @Injectable()
 export class ArtistService {
+  constructor(
+    private readonly artistRepository: ArtistRepository
+  ) {}
+
   create(createArtistDto: CreateArtistDto) {
-    return 'This action adds a new artis';
+    return this.artistRepository.create(createArtistDto);
   }
 
   findAll() {
-    return `This action returns all artist`;
+    return this.artistRepository.findAll();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} artist`;
+    return this.artistRepository.findOne(id);
   }
 
   update(id: number, updateArtistDto: UpdateArtistDto) {
-    return `This action updates a #${id} artist`;
+    return this.artistRepository.update(id, updateArtistDto);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} artist`;
+    return this.artistRepository.delete(id);
   }
 }
